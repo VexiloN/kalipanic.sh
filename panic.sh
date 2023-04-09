a4538b82c9254e77ec2bbae502548f55b06db700
VexiloN <130223430+VexiloN@users.noreply.github.com>
2023-04-09 13:13:24 +0300

    Update panic.sh

--- panic.sh
+++ panic.sh
@@ -1,3 +1,45 @@
+#!/bin/bash
+
+if [[ $(sudo ifconfig wlan0 | grep "UP") != "" ]]; then
+    echo "wlan0 the interface is already active."
+else
+    sudo ifconfig eth0 up
+    echo "eth0 the interface has been activated."
+fi
+
+echo
+
+echo "Please log in to the OpenVPN server. If you are not logged in within 10 seconds,"
+echo "the current connection service will be stopped."
+
+sleep 4
+
+echo
+
+echo "Counting down from 10..."
+
+# Checking the OpenVPN connection
+if nmcli con show --active | grep -q tun; then
+    echo "OpenVPN connection is active. Countdown cancelled."
+else
+    # Countdown
+    for (( i=10; i>=1; i-- ))
+    do
+        echo $i
+        sleep 1
+
+        # Checking the OpenVPN connection on each iteration
+        if nmcli con show --active | grep -q tun; then
+            echo "OpenVPN connection is active. Countdown cancelled."
+            break
+        fi
+    done
+
+    echo "Countdown finished."
+fi
+
+sleep 0
+
 # Network interface names to be checked
 interface1="eth0"
 interface2="wlan0"
