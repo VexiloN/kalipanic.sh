0a3baca13b1a5a5ea14c46c630f8f6da355b1aeb
VexiloN <130223430+VexiloN@users.noreply.github.com>
2023-04-09 08:08:02 +0300

    Add files via upload

---
+++ panic.sh
@@ -0,0 +1,22 @@
+# discord= Vexilon☆#1881
+# Network interface names to be checked
+interface1="eth0"
+interface2="wlan0"
+
+# Unlimited cycles
+while true
+do
+    # Checking the OpenVPN connection
+    if [[ $(nmcli con show --active | grep tun) ]]; then
+        echo "The OpenVPN connection is active. Network traffic continues."
+        sudo ifconfig $interface1 up
+        sudo ifconfig $interface2 up
+    else
+        echo "The OpenVPN connection is not active. Network traffic is being stopped."
+        sudo ifconfig $interface1 down
+        sudo ifconfig $interface2 down
+    fi
+
+    # OpenVPN connection check time
+    sleep 1
+done
